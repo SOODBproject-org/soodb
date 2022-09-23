bf00b1c4cf577a569d6d984b60ced8ef6adeb3ce
WaterNinja101 <coolwolfs8@gmail.com>
2022-09-22 22:01:30 -0400

    Added x button to notification

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -1,40 +1,58 @@
 <script lang="ts">
+    import Icon from "svelte-icon/Icon.svelte"
     import { fly, fade } from "svelte/transition"
+    import close from '$lib/icons/close.svg?raw'
 
     export let title: string
     export let text: string | null = null
     export let shown = true
-
-    // TODO: X button
 </script>
 
 {#if shown}
     <div class="notification" in:fly={{ x: 100 }} out:fade>
-        <p class="title">{title}</p>
-        {#if text}
-            <p class="text">{text}</p>
-        {/if}
+        <div class="wrapper">
+            <div class="close" on:click={() => shown = false}>
+                <Icon data={close} class="close-icon" />
+            </div>
+            <p class="title">{title}</p>
+            {#if text}
+                <p class="text">{text}</p>
+            {/if}
+        </div>
     </div>
 {/if}
 
 <style lang="scss">
     .notification {
         position: fixed;
-        right: 1em;
-        bottom: 1em;
+        right: clamp(1em, 2vw, 4em);
+        bottom: clamp(1em, 2vw, 4em);
         background: $background-2;
         padding: 1em;
         border-radius: 1em;
+
+        :global(.close-icon) {
+            position: absolute;
+            top: 5px;
+            right: 5px;
+            width: 1em;
+            height: 1em;
+            cursor: pointer;
+        }
+    }
+
+    .wrapper {
+        position: relative;
     }
 
     .title {
-        font-size: 18px;
+        font-size: 22px;
         margin-top: 0;
         margin-bottom: 0.5em;
     }
 
     .text {
-        font-size: 14px;
+        font-size: 18px;
         margin: 0;
     }
 </style>
